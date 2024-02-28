e41cb166b2c76e4703f0984f303c76bd2336221f
Diego Ferreiro Alonso <dferra01@estudiantes.unileon.es>
2024-02-28 12:30:58 +0100

    Continuación Prácticas

---
+++ Practica 4/Ejercicio 2/suma_n_numeros.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void suma_n_numeros(int num) {
+    int i;
+    for ()
+    
+}
+
+int main() {
+
+    int num;
+
+    printf("Escriba un número: ");
+    scanf("%d", num);
+
+    if (num >= 0) {
+        printf("El número es positivo");
+    } else {
+        printf("Error, introduzca un número positivo");
+        scanf("%d", num);
+    }
+
+    suma_n_numeros(num);
+}
