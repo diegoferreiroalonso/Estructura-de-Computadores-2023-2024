35630f7ce214ad9455af47d2af4ac9c4655ea95d
Diego Ferreiro Alonso <dferra01@estudiantes.unileon.es>
2024-03-10 00:09:04 +0100

    Update Practica 7

--- Practica 7/Ejercicio 1/sumaElementoos.c
+++ Practica 7/Ejercicio 1/sumaElementoos.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-int vector(int vec[]) {
+int vector(int vec[10]) {
 
-    int tamaño;
-    for (int i = 0; i < tamaño; i++) {
+    for (int i = 0; i < 10; i++) {
         vec[i] = rand() % (10 + 1);
     }
 }
 
-int suma() {
+int suma(int vec[10]) {
 
+    int suma = 0;
+    for (int i = 0; i < 10; i++) {
+        suma += vec[i];
+    }
+
+    return suma;
 }
 
 int main() {
@@ -22,12 +27,12 @@ int main() {
 
     vector(vec);
 
-    int resultado = suma();
+    int resultado = suma(vec);
 
     printf("\nSuma de todos los elementos del vector: %d", resultado);
     printf("\nElementos del vector: ");
     for (int i = 0; i < 10; i++) {
-        printf("")
+        printf("%d", vec[i]);
     }
 
     return 0;
