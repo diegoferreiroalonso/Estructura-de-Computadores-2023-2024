90de218e233edf23d7b0713b832b9ba3472bbd7b
Diego Ferreiro Alonso <dferra01@estudiantes.unileon.es>
2024-03-19 20:13:18 +0100

    Comentarios

--- Practica 8/Ejercicio 1/DNI.py
+++ Practica 8/Ejercicio 1/DNI.py
@@ -1,12 +1,13 @@
-def main():
+def main(): # Define una nueva función llamada "main"
 
-    dni = int(input("\nEscriba su DNI (sin letra): "))
+    print("\nEscriba su DNI (sin letra): ") # Imprime el mensaje en la consola
     
-    res = dni % 23
+    dni = int(input()) # Recibe datos del usuario
     
+    res = dni % 23 # Calcula esl resto y lo guarda en una variable
     
-    lista = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
+    lista = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'] # Creación de la lista ordenada (se cuentan las posiciones empezando en 0)
     
-    print(f"\nLa letra del DNI es: {lista[res]}")
+    print(f"\nLa letra del DNI es: {lista[res]}") # Imprime el mensaje y la lista (que en este caso solo será una letra) en la consola
     
-main()
+main() # Llamada a la función
--- Practica 8/Ejercicio 2/Lista.py
+++ Practica 8/Ejercicio 2/Lista.py
@@ -1,16 +1,16 @@
-def main():
+def main(): # Define una nueva función llamada "main"
     
-    lista1 = [-4, -2, 0, 2, 4]
+    lista1 = [-4, -2, 0, 2, 4] # Creación de la lista ordenada con valores predeterminados
     
-    print("\nLa lista uno contiene: ")
-    print (lista1)
+    print("\nLa lista uno contiene: ") # Imprime el mensaje en la consola
+    print (lista1) # Imprime la lsta creada antes "lista1" en la consola
     
-    lista2 = []
-    for i in range(len(lista1)):
-        if lista1[i] >= 0: 
-            lista2.append(lista1[i])
+    lista2 = [] # Creación una lista ordenada vacía
+    for i in range(len(lista1)): # Crea el bucle for con un determinado número de iteraciones, el cual viene dado por la longitud de la lista1 (es decir, las posiciones totales de la lista, empezando en la 0)
+        if lista1[i] >= 0: # Crea el condicional if (si) que ejecuta el codigo si se cumple la condición, que en este caso es que la lista uno en cada iteración de i, es decir, en cada posición sea mayor que 0 
+            lista2.append(lista1[i]) # Se le añade de la lista1 los elemntos que esten en las posiciones que pasaron la condición anterior a la lista2 en última posición (solo que al estar vacio se empezaran a añadir desde 0)
     
-    print("\nLa lista dos contiene: ")
-    print (lista2)
+    print("\nLa lista dos contiene: ") # Imprime el mensaje en la consola
+    print (lista2) # Imprime la lista "lista2" en la cosola
     
-main()
+main() # Llama a la función
