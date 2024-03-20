a22c21833ac8b70d0f988bfa6407a6775965baf8
Diego Ferreiro Alonso <dferra01@estudiantes.unileon.es>
2024-03-20 11:48:17 +0100

    Prueba 5%

---
+++ Prueba Python/Prueba.py
@@ -0,0 +1,27 @@
+def comprobación(a, b, c):
+    
+    print("\nLos números son:")
+    for i in range (a, b + 1, c):
+        print(i)
+    
+def main():
+    
+    a = int(input("\nIntroduce un número: "))  
+    
+    b = int(input("\nIntroduce un número: ")) 
+    
+    c = int(input("\nIntroduce un número: ")) 
+    
+    while (a >= b or c < 0):
+        
+        print("\nError, intoduce unos números válidos")
+        
+        a = int(input("\nIntroduce un número correcto: "))  
+    
+        b = int(input("\nIntroduce un número correcto: ")) 
+    
+        c = int(input("\nIntroduce un número mayor que 1: ")) 
+        
+    comprobación (a, b, c)
+
+main()
