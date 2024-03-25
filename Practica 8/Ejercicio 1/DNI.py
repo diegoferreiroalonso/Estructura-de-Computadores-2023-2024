fb44b31d56d07b41fd15fdd3740ea60d0ab34a54
Diego Ferreiro Alonso <dferra01@estudiantes.unileon.es>
2024-03-25 20:08:41 +0100

    Comentarios

--- Practica 8/Ejercicio 1/DNI.py
+++ Practica 8/Ejercicio 1/DNI.py
@@ -1,10 +1,10 @@
-def main(): # Define una nueva función llamada "main"
+def main(): # Define [def] una función [...()] llamada "main" sin argumentos
 
-    print("\nEscriba su DNI (sin letra): ") # Imprime el mensaje en la consola
+    print("\nEscriba su DNI (sin letra): ") # Imprime [print()] el mensaje [("\n...")] en la consola con un salto de linea [("\n")]
     
-    dni = int(input()) # Recibe datos del usuario
+    dni = int(input()) # Guarda los datos del usuario [input()] de tipo númericos [int()] en la variable [... =] llamada "dni"
     
-    res = dni % 23 # Calcula esl resto y lo guarda en una variable
+    res = dni % 23 # Calcula el resto [%] de la variable "dni" entre el número entero 23 y lo guarda en una variable [... =] llamada "res"
     
     lista = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'] # Creación de la lista ordenada (se cuentan las posiciones empezando en 0)
     
