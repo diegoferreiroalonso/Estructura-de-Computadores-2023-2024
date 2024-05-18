cce072fb7f86241df0fb51641ab7dad16fc86269
Diego Ferreiro Alonso <dferra01@estudiantes.unileon.es>
2024-05-18 17:49:32 +0200

    Assembly

--- Practicas/Practica 13/Ejercicio 1/Llamadas.asm
+++ Practicas/Practica 13/Ejercicio 1/Llamadas.asm
@@ -1,30 +1,39 @@
 
-    .data
+    .data # Comienza el segmento de datos (.data)
     
-datos:
-    .word 3, 7
+datos: # Declara los datos (datos:)
+    .word 3, 7 # Declara datos de tipo word (.word) (32 bits), en este caso los números 3 y 7 separados por una coma
 
-cad1: .string "Introduzca el primer número: "
-cad2: .string "Introduzca el segundo número: "
-cad3: .string "Introduzca el tercer número: "
-cad4: .string "Introduzca el cuarto número: "
+cad1: .string "Introduzca el primer número:" # Declara una cadena de caracteres (.string) llamada "cad1" (cad1: ) con el contenido "Introduzca el primer número:"
+cad2: .string "Introduzca el segundo número:" # Declara una cadena de caracteres (.string) llamada "cad2" (cad2: ) con el contenido "Introduzca el segundo número:"
+cad3: .string "Introduzca el tercer número:" # Declara una cadena de caracteres (.string) llamada "cad3" (cad3: ) con el contenido "Introduzca el tercer número:"
+cad4: .string "Introduzca el cuarto número:" # Declara una cadena de caracteres (.string) llamada "cad4" (cad4: ) con el contenido "Introduzca el cuarto número:"
+espa: .string "		"
 
-    .globl main
-    .text
+    .globl main # Declara la directiva de etiqueta global (.globl) (accesible desde ortros archivos)
+    .text # Comienza el segmento de texto (.text)
 
-main:
+main: # Declara la función principal del programa (main:)
 
-    li a7, 4
-    li a0, cad1
-    ecall
+    li a7, 4 # Llamada al código 4 de a7 que realiza la función de print_string
+    la a0, cad1 # Imprime la cadena "cad1" que está en la dirección a0 (puntero a0 -> cad1)
+    ecall # Llamada al sistema
     
-    li a7, 5
-    ecall
+    li a7, 4 # Llamada al código 4 de a7 que realiza la función de print_string
+    la a0, espa # Imprime la cadena "espa" que está en la dirección a0 (puntero a0 -> espa)
+    ecall # Llamada al sistema
+    
+    li a7, 5 # Llamada al código 5 de a7 que realiza la función de  read_int
+    ecall # Llamada al sistema
     
     mv t0, a0
     
     li a7, 4
-    li a0, cad2
+    la a0, cad2
+    ecall
+    
+    li a7, 4
+    la a0, espa
     ecall
     
     li a7, 5
@@ -33,7 +42,11 @@ main:
     mv t1, a0
     
     li a7, 4
-    li a0, cad3
+    la a0, cad3
+    ecall
+    
+    li a7, 4
+    la a0, espa
     ecall
     
     li a7, 5
@@ -42,7 +55,11 @@ main:
     mv t2, a0
 
     li a7, 4
-    li a0, cad4
+    la a0, cad4
+    ecall
+    
+    li a7, 4
+    la a0, espa
     ecall
     
     li a7, 5
@@ -56,10 +73,18 @@ main:
     mv a0, t4
     ecall
     
+    li a7, 4
+    la a0, espa
+    ecall
+    
     sub t5, t2, t3
     
-    li a7, 1
+    li a1, 1
     mv a0, t5
+    ecall
+    
+    li a7, 4
+    la a0, espa
     ecall
     
     add a1, t0, t1
@@ -70,20 +95,32 @@ main:
     mv a0, a3
     ecall
     
+    li a7, 4
+    la a0, espa
+    ecall
+    
     sll a4, t0, t0
     
     li a7, 1
     mv a0, a4
     ecall
     
+    li a7, 4
+    la a0, espa
+    ecall
+    
     la a0, datos
-    lw a5, 0(t0)
-    lw a6, 4(t6)
+    lw a5, 0(a0)
+    lw a6, 4(a0)
     
     rem a7, a6, a5
     
     li a7, 1
     mv a0, a7
+    ecall
+    
+    li a7, 4
+    la a0, espa
     ecall
 
     li a7, 10
